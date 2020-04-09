5cee9dd0121dbb380068d8075c4a2b4da2c87a06
Cesar Alarcon <ciam042@gmail.com>
2020-04-08 19:12:06 -0500

    Updates

---
+++ precache-manifest.e3648db3bdec1bf12a0a6aa3826f2d99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e7533b24cf0097ee53e705fe28ce768",
+    "url": "/ciam-pokedex/index.html"
+  },
+  {
+    "revision": "4ccbdce6417cb0ab0cef",
+    "url": "/ciam-pokedex/static/css/main.149f21ae.chunk.css"
+  },
+  {
+    "revision": "86dce0d3649e243cd363",
+    "url": "/ciam-pokedex/static/js/2.5d2a1e4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ciam-pokedex/static/js/2.5d2a1e4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ccbdce6417cb0ab0cef",
+    "url": "/ciam-pokedex/static/js/main.50e12676.chunk.js"
+  },
+  {
+    "revision": "c045a0b0db03ea738511",
+    "url": "/ciam-pokedex/static/js/runtime-main.0414636f.js"
+  }
+]);
